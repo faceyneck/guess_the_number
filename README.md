0f47128e4b414c0932b7521b3f0317d26415e027
RJ <whiteymccracker@mail.ru>
2020-11-10 15:29:03 -0700

    Program written and works well enough. No error handling at the moment. I may add that at another time.

--- README.md
+++ README.md
@@ -1,26 +1,6 @@
-
-<head>
-<style>
-body {
-    background-color: black;
-    color: blue;
-}
-
-</style>
-</head>
-
-
-
-<body>
-
-<h1>BANANA</h1>
-
-
 # Guess The Number
 This is a very simple Python 3 program which chooses a number the "player" is supposed to guess.
 
 It can be found in Al Swigiart's book, **INVENT YOUR OWN COMPUTER GAMES WITH PYTHON.**
 
-I don't know how much more there is to say. It's kinda the Karen of video games.
-
-</body>
+I don't know how much more there is to say. It's kinda the Karen of video games, but it involves a lot of neat programming thingies that I'm interested in improving. :-D
---
+++ guess_the_number.py
@@ -0,0 +1,41 @@
+'''
+This is a program that chooses a number between 1 and 20 with the "random" function call.
+The player has 6 attempts to guess the correct number.
+I usually like to place absurdly vulgar things into comments, but I'm fresh out of
+inappropriate ideas, so just use your imagination and assume I said something about
+sex, drugs and possibly rock-n-roll.
+'''
+
+from random import randint
+
+# Generate the random number
+random_number = randint(1, 20)
+
+#begin the guessing counter at 0
+guesses_taken = 0
+
+print("Yo, playa! What\'s your name, you saucy upstart?")
+player_name = input()
+
+print("Well hey uh... don't freak out or anything, " + player_name + ", but I'm thinking of a number between 1 and 20.")
+print("You have 6 chances to guess the correct number, or every single kitten on Earth will die of feline AIDS.")
+
+for guesses_taken in range (6):
+    print("Take a guess, kiddo! No pressure.")
+    guess = input()
+    guess = int(guess)
+
+    if guess < random_number:
+        print("Your guess is too low, " + player_name + " try again.")
+
+    if guess > random_number:
+        print("Whoa hey now!" + " Slow down, cowboy. Your guess is too high, try again.")
+
+    if guess == random_number:
+        break
+if guess == random_number:        
+    guesses_taken = str(guesses_taken + 1)    
+    print("Well shucks, player! You done guessed the right number in " + guesses_taken + "! \n Great job, the kitties are a-okay")
+if guess != random_number:
+    random_number = str(random_number)
+    print("I guess you don't like healthy kittens. The number I was thinking of was: " + random_number + ". \nPoor kitties!")
